e35b14d4e81158c433d478b3c3d136ab1d04b6e8
Gina Pultorak Scarpello <50841963+ginapult@users.noreply.github.com>
2019-10-01 17:15:38 -0700

    update auth

--- src/app.js
+++ src/app.js
@@ -1,5 +1,36 @@
 import React from 'react';
 
-import Auth from './Components/auth/auth.js';
-import LoginContext from './Components/auth/context.js';
+import Auth from './components/auth/auth.js';
+import Login from './components/auth/login.js';
+import LoginProvider from './components/auth/context.js';
 
+const Read = props => {
+  return (
+    <Auth capability='read'>
+      <span>Read</span>
+    </Auth>
+  );
+};
+
+const Update = props => {
+  return (
+    <Auth capability='update'>
+      <span>Update</span>
+    </Auth>
+  );
+}
+
+class App extends React.Component {
+  render () {
+    return (
+      <LoginProvider>
+        <Login />
+        <hr />
+        <Read />
+        <Update />
+      </LoginProvider>
+    );
+  }
+}
+
+export default App;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import App from '../../app.js';
 
 const Main = () => {
   return (
     <main>
       <h2>Let's make some magic happen!</h2>
+      <App />
     </main>
   );
 };
